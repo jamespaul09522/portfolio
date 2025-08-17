559f20863f34ecfb187f4b37eff38cdca286f0b1
jamespaul09522 <98757018+jamespaul09522@users.noreply.github.com>
2025-08-17 11:57:35 +0530

    Update background-line.tsx

--- src/app/background-line.tsx
+++ src/app/background-line.tsx
@@ -9,9 +9,6 @@ import { LinkPreview } from "@/components/ui/link-preview";
 export function BackgroundLinesDemo() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-      <p>Follow me on  <LinkPreview url="https://www.linkedin.com/in/james-paul-57014122a/" className="font-bold">
-          LinkedIn
-        </LinkPreview>{" "}</p>
       <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Hi, This is <span className="text-blue-500">James!</span>
       </h2>
